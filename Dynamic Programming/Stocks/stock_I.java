27fea99331c347544efb4446a68411fce8270e91
AviralKhanna <aviral.projects24@gmail.com>
2025-10-07 18:56:20 +0530

    stocks

---
+++ Dynamic Programming/Stocks/stock_I.java
@@ -0,0 +1,16 @@
+
+public class stock_I {
+    public static void main(String[]args)
+    {
+    int[] arr={7,1,2,4,3,6,3};
+    int mini= arr[0];
+    int profit=0;
+    for(int i=1; i<arr.length;i++)
+    {
+        int cost= arr[i]-mini;
+        profit= Math.max(profit,cost);
+        mini= Math.min(mini,arr[i]);
+    }
+    System.out.println(profit);
+}
+}
